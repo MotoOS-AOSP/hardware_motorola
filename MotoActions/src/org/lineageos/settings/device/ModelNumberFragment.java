0f10cda51a8b625088228511800359e5d5f05dec
seba3567 <sebastian26481@gmail.com>
2024-06-14 18:05:47 -0400

    rework moto actions

--- MotoActions/src/org/lineageos/settings/device/ModelNumberFragment.java
+++ MotoActions/src/org/lineageos/settings/device/ModelNumberFragment.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2015-2016 The CyanogenMod Project
- * Copyright (C) 2017-2022 The LineageOS Project
+ * Copyright (C) 2023 ArrowOS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,26 +17,20 @@
 package org.lineageos.settings.device;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 
-public class ActionsPreferenceFragment extends PreferenceFragment {
-
-    private static final String KEY_ACTIONS_CATEGORY = "actions_key";
+public class ModelNumberFragment extends PreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.actions_panel);
-    }
+        addPreferencesFromResource(R.xml.model_number_panel);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            getActivity().onBackPressed();
-            return true;
-        }
-        return false;
+        findPreference(MotoActionsSettings.MODEL_NUMBER_KEY)
+                .setSummary(MotoActionsSettings.getModelNumberString(getActivity()));
+
+        findPreference(MotoActionsSettings.CARRIER_KEY)
+                .setSummary(MotoActionsSettings.getCarrierString(getActivity()));
     }
 }
